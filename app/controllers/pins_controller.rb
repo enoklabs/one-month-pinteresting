d98af92be3fe4f83509d51da2b91625cb90966bc
Enok Madrid <enokslaboratory@gmail.com>
2015-02-09 12:09:34 -0800

    change build to new

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -20,7 +20,7 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = current_user.pins.build(pin_params)
+    @pin = current_user.pins.new(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
